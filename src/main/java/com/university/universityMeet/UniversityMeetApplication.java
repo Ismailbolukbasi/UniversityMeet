c63b3cd9684ef80993b7fcc1a086de608ca56606
Ismailbolukbasi <ismailbolukbasi@ogr.eskisehir.edu.tr>
2023-08-11 13:53:11 +0300

    initilaze project

---
+++ src/main/java/com/university/universityMeet/UniversityMeetApplication.java
@@ -0,0 +1,13 @@
+package com.university.universityMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMeetApplication.class, args);
+	}
+
+}
